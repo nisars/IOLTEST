a1c9917c4eb3b12ba1037276e4eaa1b58a4617cb
nisar <nisar.s@illusions-online.com>
2017-01-11 07:43:14 +0100

    Project  Saleem Deleted  from  GIT  SERVER

--- Cisco/saleem/saleem/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace saleem.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- Cisco/saleem/saleem/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="saleem.Global" Language="C#" %>
