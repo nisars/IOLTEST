ea47922338fd4ad0687692871556bfa7f0d3e8a0
nisar <nisar.s@illusions-online.com>
2017-01-11 07:35:54 +0100

    Project  Uploaded to GIT  Server

---
+++ Cisco/saleem/saleem/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace saleem.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Cisco/saleem/saleem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="saleem.Global" Language="C#" %>
