fb838319a162b24f860e24e709b85396acd6304a
nisar <nisar.s@illusions-online.com>
2017-01-11 08:34:58 +0100

    test  project

---
+++ Cisco/Saleem/Saleem/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Saleem.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Cisco/Saleem/Saleem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Saleem.Global" Language="C#" %>
